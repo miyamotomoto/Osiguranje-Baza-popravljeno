2ef435fc8c623338637a5ceafd3090c724ed360f
dimitrije <dimitrije.stojcic@elfak.rs>
2026-07-02 10:20:03 +0200

    Add project files.

---
+++ Controllers/Angazovana_osobaController.cs
@@ -0,0 +1,99 @@
+﻿using DatabaseAccses.DTOs;
+using DatabaseAccses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Osiguranje_Baza_popravljeno.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Angazovana_osobaController:ControllerBase
+    {
+        [HttpGet]
+        [Route("PrikaziAngazovanu")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult GetAngazovanu(int id_klijenta)
+        {
+            try
+            {
+                return new JsonResult(DataProvider.vratiAngazovanu(id_klijenta));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [Route("DodajAngazovanu")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult DodajKlijenta([FromBody] Angazovana_osobaView klijent)
+        {
+            var Tip_klijenta = new HashSet<string>
+            {
+                "Agent",
+                "Lekar_konsultant",
+                "Pravnik",
+                "Procenitelj",
+            };
+
+
+            try
+            {
+                if (!Tip_klijenta.Contains(klijent.tip_osobe))
+                {
+                    return BadRequest("Uneli ste nevalidnu klasu!");
+                }
+
+
+                DataProvider.dodajAngazovanu(klijent);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+
+        [HttpPut]
+        [Route("PromeniAngazovanu")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult PromeniAngazovanu([FromBody] Angazovana_osobaView avatar)
+        {
+            try
+            {
+                DataProvider.azurirajAngazovanu(avatar);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+        [HttpDelete]
+        [Route("IzbrisiAngazovanu/{idKlijenta}")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult IzbrisiAvatara(int idKlijenta)
+        {
+            try
+            {
+                DataProvider.obrisiAngazovanu(idKlijenta);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+
+
+
+
+
+    }
+}
---
+++ Controllers/KlijentController.cs
@@ -0,0 +1,115 @@
+﻿using DatabaseAccses;
+using DatabaseAccses.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Osiguranje_Baza_popravljeno.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class KlijentController : ControllerBase
+    {
+        [HttpGet]
+        [Route("PrikaziKlijenta")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult GetKlijent(int id_klijenta)
+        {
+            try
+            {
+                return new JsonResult(DataProvider.vratiKlijenta(id_klijenta));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [Route("DodajKlijent/{Tip_Klijenta}")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult DodajKlijenta([FromBody] CreateKlijentView klijent, string Tip_Klijenta)
+        {
+            var Tip_klijenta = new HashSet<string>
+            {
+                "Fizicko_lice",
+                "Javna_institucija",
+                "Pravno_lice",
+
+            };
+
+
+            try
+            {
+                if (!Tip_klijenta.Contains(Tip_Klijenta))
+                {
+                    return BadRequest("Uneli ste nevalidnu klasu!");
+                }
+
+
+                DataProvider.dodajKlijenta(klijent,Tip_Klijenta);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+
+        [HttpPut]
+        [Route("PromeniKlijent")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult PromeniKlijenta([FromBody] CreateKlijentView avatar)
+        {
+            try
+            {
+                DataProvider.azurirajKlijenta(avatar);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+        [HttpDelete]
+        [Route("IzbrisiKlijenta/{idKlijenta}")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult IzbrisiAvatara(int idKlijenta)
+        {
+            try
+            {
+                DataProvider.obrisiKlijenta(idKlijenta);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
